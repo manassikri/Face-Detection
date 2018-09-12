e02c067547963724086831b3bd3c1204562d02d9
manassikri <43198579+manassikri@users.noreply.github.com>
2018-09-12 12:31:33 +0530

    Add files via upload

---
+++ realTimeVideo2.py
@@ -0,0 +1,31 @@
+import cv2
+
+
+cam = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier('C:\opencv\sources\data\haarcascades\haarcascade_frontalface_default.xml')
+cv2.namedWindow("output", cv2.WINDOW_NORMAL)
+while True:
+    
+    ret_val,img=cam.read(1)
+    
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    th =50.0
+
+
+    faces = face_cascade.detectMultiScale(gray, 1.1, 12)
+    for (x,y,w,h) in faces:
+        img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),5)
+        if (w*h > th):
+            print "face deteced"
+
+        
+    cv2.imshow('output',img)
+    
+
+
+
+
+    
+    if cv2.waitKey(1)==27:
+        break
+cv2.destroyAllWindows()
